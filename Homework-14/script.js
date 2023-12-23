84934240eb112dc5d69bb052bba71b27d4576f29
MM <76992756+UnknoWnCoder998@users.noreply.github.com>
2023-12-23 19:09:11 +0500

    Add files via upload

---
+++ Homework-14/script.js
@@ -0,0 +1,13 @@
+let first = Number(document.querySelector('#first'))
+let second = Number(document.querySelector('#second'))
+let firstDigit = Number(prompt('first digit'))
+let secondDigit = Number(prompt('second digit'))
+
+console.log(firstDigit + secondDigit);
+
+// document.querySelector('#plus').addEventListener('click', function(e) {
+//     let result = first + second
+//     console.log(result);
+    
+
+// })
